af9ba2a9a8401a984e83e9aa45c53b79bf35da08
Bernd Amend <bernd.amend@gmail.com>
2017-10-28 09:18:13 +0200

    add new plugin updater, to perform remote update

---
+++ plugins/updater.py
@@ -0,0 +1,21 @@
+from IPlugin import IPlugin
+import subprocess
+
+# add proper error handling
+class Updater(IPlugin):
+
+    def __init__(self, config):
+        return
+
+    def handlemessage(self, bot, msg):
+        if msg.text.lower() == 'update yourself!':
+            output = subprocess.check_output("git pull --ff-only", shell=True)
+            msg.reply_text(output.decode("utf-8"))
+            return True
+        else:
+            return False
+    
+    def helpmessage(self):
+        return {"update yourself!": "get the newest version of minion_bot"}
+
+__export__ = Updater
